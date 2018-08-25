6f0af7721851500e02783034842b952a41498854
Damian Figarski <damian.figarski@doddlecode.com>
2018-08-25 20:08:02 +0200

    Factory method project

--- src/test/java/FactoryMethodTest.java
+++ src/test/java/FactoryMethodTest.java
@@ -8,7 +8,6 @@ import com.doddlecode.WrongCountryException;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class FactoryMethodTest {
